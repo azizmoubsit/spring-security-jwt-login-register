f986e1c78a81c915075302727645cb284c7e56ab
azizmoubsit <azizmobsit@gmail.com>
2023-07-31 23:02:41 +0100

    initial commit

---
+++ src/main/java/com/socialmedia/SocialMediaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.socialmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialMediaSpringApplication.class, args);
+    }
+
+}
